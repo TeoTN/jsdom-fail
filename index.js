fe7ce60c159d7b53abed6227fe58972718a068d3
Piotr Staniów <staniowp@gmail.com>
2017-01-04 20:14:32 +0100

    Update

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
  * Created by Piotr Staniów on 04.01.2017.
  */
 
-const Cookies = require('js-cookie');
-Cookies.set('coo', 'foo');
+document.cookie = 'coo=foo';
--- test/test.js
+++ test/test.js
@@ -22,7 +22,12 @@ describe('Cookies test', () => {
         document.body.appendChild(scriptEl);
     });
 
-    it('should set and get cookie', () => {
+    it('should set and get cookie with my favourite lib', () => {
         expect(Cookies.get('coo')).to.be.equal('foo');
+    });
+
+    it('should set and get cookie at all', () => {
+        expect(window.document.cookie).to.contain('coo');
+        console.log(window.document.cookie);
     })
 });
